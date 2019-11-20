b055e92a6dc1972a8a6f7b96441fa64560386a2a
MichaelHYJiang <google-dl-platform@googlegroups.com>
2019-11-20 00:30:21 +0000

    generate file lists from histogram equalized data

--- generate_file_list.py
+++ generate_file_list.py
@@ -1,7 +1,7 @@
 import glob, os
 import numpy as np
 
-files = os.listdir('./0_data/train/')
+files = os.listdir('./0_data/train_he/')
 files.sort()
 
 assert len(files) == 179
@@ -13,22 +13,22 @@ with open('file_list', 'w') as f:
     for i in index[:125]:
         file = files[i]
         name = file[:-4]
-        in_path = './0_data/train/' + file
-        gt_path = './0_data/gt/' + file
+        in_path = './0_data/train_he/' + file
+        gt_path = './0_data/gt_he/' + file
         f.write(name + ' ' + gt_path + ' ' + in_path + '\n')
 
 with open('valid_list', 'w') as f:
     for i in index[125:152]:
         file = files[i]
         name = file[:-4]
-        in_path = './0_data/train/' + file
-        gt_path = './0_data/gt/' + file
+        in_path = './0_data/train_he/' + file
+        gt_path = './0_data/gt_he/' + file
         f.write(name + ' ' + gt_path + ' ' + in_path + '\n')
 
 with open('test_list', 'w') as f:
     for i in index[152:]:
         file = files[i]
         name = file[:-4]
-        in_path = './0_data/train/' + file
-        gt_path = './0_data/gt/' + file
+        in_path = './0_data/train_he/' + file
+        gt_path = './0_data/gt_he/' + file
         f.write(name + ' ' + gt_path + ' ' + in_path + '\n')
