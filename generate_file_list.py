361963747daef6324a368eac5241c08f76b271a2
Haiyang Jiang <450199156@qq.com>
2019-11-20 18:26:10 -0800

    generate pairs from input to HE_gt

--- generate_file_list.py
+++ generate_file_list.py
@@ -1,7 +1,10 @@
 import glob, os
 import numpy as np
 
-files = os.listdir('./0_data/train_he/')
+train_dir = 'train'
+gt_dir = 'gt_he'
+
+files = os.listdir('./0_data/' + train_dir + '/')
 files.sort()
 
 assert len(files) == 179
@@ -13,22 +16,22 @@ with open('file_list', 'w') as f:
     for i in index[:125]:
         file = files[i]
         name = file[:-4]
-        in_path = './0_data/train_he/' + file
-        gt_path = './0_data/gt_he/' + file
+        in_path = './0_data/' + train_dir + '/' + file
+        gt_path = './0_data/' + gt_dir + '/' + file
         f.write(name + ' ' + gt_path + ' ' + in_path + '\n')
 
 with open('valid_list', 'w') as f:
     for i in index[125:152]:
         file = files[i]
         name = file[:-4]
-        in_path = './0_data/train_he/' + file
-        gt_path = './0_data/gt_he/' + file
+        in_path = './0_data/' + train_dir + '/' + file
+        gt_path = './0_data/' + gt_dir + '/' + file
         f.write(name + ' ' + gt_path + ' ' + in_path + '\n')
 
 with open('test_list', 'w') as f:
     for i in index[152:]:
         file = files[i]
         name = file[:-4]
-        in_path = './0_data/train_he/' + file
-        gt_path = './0_data/gt_he/' + file
+        in_path = './0_data/' + train_dir + '/' + file
+        gt_path = './0_data/' + gt_dir + '/' + file
         f.write(name + ' ' + gt_path + ' ' + in_path + '\n')
