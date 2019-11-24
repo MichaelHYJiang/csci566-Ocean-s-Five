05939b937dfb807bc0a6a2cbf3803a5952c75a8d
Haiyang Jiang <450199156@qq.com>
2019-11-24 18:20:23 +0900

    add GAN's losses and their writers

--- train.py
+++ train.py
@@ -66,6 +66,8 @@ def demosaic(in_vid, converter=cv2.COLOR_BayerGB2BGR):
 def crop(raw, gt_raw, start_frame=0):
     # inputs must be in a form of [batch_num, frame_num, height, width, channel_num]
     tt = start_frame
+    W = raw.shape[3]
+    H = raw.shape[2]
     xx = np.random.randint(0, W - CROP_WIDTH)
     yy = np.random.randint(0, H - CROP_HEIGHT)
 
@@ -103,32 +105,46 @@ def validate(in_path, gt_path, sess, G_loss, out_image, in_image, gt_image):
 
     input_patch, gt_patch = flip(input_patch, gt_patch)
     input_patch = np.minimum(input_patch, 1.0)
-    loss, output = sess.run([G_loss, out_image], feed_dict={in_image: input_patch, gt_image: gt_patch})
+    loss = sess.run(G_loss, feed_dict={in_image: input_patch, gt_image: gt_patch})
     return loss
+
+
+def sigmoid_cross_entropy_loss(labels, logits):
+    loss = tf.nn.sigmoid_cross_entropy_with_logits(labels = labels, logits = logits)
+    return tf.reduce_mean(loss)
 
 
 def main():
     sess = tf.Session()
-    in_image = tf.placeholder(tf.float32, [None, CROP_FRAME, None, None, 4])
-    gt_image = tf.placeholder(tf.float32, [None, CROP_FRAME, None, None, 3])
-    with tf.variable_scope('gen'):
-        out_image = network(in_image)
+    in_image = tf.placeholder(tf.float32, [1, CROP_FRAME, CROP_HEIGHT, CROP_WIDTH, 4])
+    gt_image = tf.placeholder(tf.float32, [1, CROP_FRAME, CROP_HEIGHT * 2, CROP_WIDTH * 2, 3])
+    out_image = network(in_image)
     if DEBUG:
         print '[DEBUG] out_image shape:', out_image.shape
 
-    with tf.variable_scope('dis'):
-        loss_out_image = loss_network(out_image)
-        loss_gt_image = loss_network(gt_image)
+    logits_out_images = loss_network(out_image[0])[:, 0]
+    logits_gt_images = loss_network(gt_image[0])[:, 0]
+    # labels = [0] * CROP_FRAME + [1] * CROP_FRAME
+    label_ones = tf.constant([0.0] * CROP_FRAME)
+    label_zeros = tf.constant([1.0] * CROP_FRAME)
+    dis_loss_op = sigmoid_cross_entropy_loss(label_ones, logits_gt_images) + sigmoid_cross_entropy_loss(label_zeros, logits_out_images)
+    gen_loss_op = sigmoid_cross_entropy_loss(label_ones, logits_out_images)
+    # dis_loss_op = -tf.reduce_mean(tf.abs(logits_out_images - logits_gt_images))
+    # gen_loss_op = -dis_loss_op
     
     
-    G_loss = tf.reduce_mean(tf.abs(loss_out_image - loss_gt_image))
+    D_loss =  dis_loss_op #1e-7 * tf.reduce_mean(tf.abs(loss_out_image - loss_gt_image))
+    L1_loss = tf.reduce_mean(tf.abs(out_image - gt_image))
+    G_loss = gen_loss_op + L1_loss
     v_loss = tf.Variable(0.0)
 
     # tensorboard summary
     tf.summary.scalar('loss', v_loss)
     # tf.summary.scalar('validation loss', v_loss)
     summary_op = tf.summary.merge_all()
-    writer = tf.summary.FileWriter(os.path.join(LOGS_DIR, TRAIN_LOG_DIR), graph=tf.get_default_graph())
+    writer = tf.summary.FileWriter(os.path.join(LOGS_DIR, TRAIN_LOG_DIR, 'l1_loss'), graph=tf.get_default_graph())
+    writer_g = tf.summary.FileWriter(os.path.join(LOGS_DIR, TRAIN_LOG_DIR, 'g_loss'), graph=tf.get_default_graph())
+    writer_d = tf.summary.FileWriter(os.path.join(LOGS_DIR, TRAIN_LOG_DIR, 'd_loss'), graph=tf.get_default_graph())
     writer_val = tf.summary.FileWriter(os.path.join(LOGS_DIR, VAL_LOG_DIR), graph=tf.get_default_graph())
 
     t_vars = tf.trainable_variables()
@@ -136,7 +152,7 @@ def main():
     dis_var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'dis')
     gen_var_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'gen')
     
-    D_opt = tf.train.AdamOptimizer(learning_rate=lr).minimize(-G_loss, var_list=dis_var_list)
+    D_opt = tf.train.AdamOptimizer(learning_rate=lr).minimize(D_loss, var_list=dis_var_list)
     
     G_opt = tf.train.AdamOptimizer(learning_rate=lr).minimize(G_loss, var_list=gen_var_list)
 
@@ -221,8 +237,16 @@ def main():
 
                 input_patch = np.minimum(input_patch, 1.0)
 
-                _, G_current, output = sess.run([D_opt, G_loss, out_image], feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
-                _, G_current, output = sess.run([G_opt, G_loss, out_image], feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
+                # output0 = sess.run(out_image, feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
+                _, G_current_d = sess.run([D_opt, D_loss], feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
+                # output1 = sess.run(out_image, feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
+                # assert 0.0 == abs(output0 - output1).sum()
+                # lg0 = sess.run(logits_gt_images, feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
+                _, G_current_g, output = sess.run([G_opt, G_loss, out_image], feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
+                G_current = sess.run(L1_loss, feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
+                # lo = sess.run(logits_out_images, feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
+                # lg = sess.run(logits_gt_images, feed_dict={in_image: input_patch, gt_image: gt_patch, lr: learning_rate})
+                # assert 0.0 == abs(lg - lg0).sum()
                 output = np.minimum(np.maximum(output, 0), 1)
                 g_loss[ind] = G_current
 
@@ -232,6 +256,10 @@ def main():
                 # save loss
                 summary = sess.run(summary_op, feed_dict={v_loss:G_current})
                 writer.add_summary(summary, count)
+                summary = sess.run(summary_op, feed_dict={v_loss:G_current_g})
+                writer_g.add_summary(summary, count)
+                summary = sess.run(summary_op, feed_dict={v_loss:G_current_d})
+                writer_d.add_summary(summary, count)
                 count += 1
 
                 if save_results and start_frame in SAVE_FRAMES:
@@ -243,7 +271,7 @@ def main():
 
 
 
-                print("%d %d Loss=%.8f Time=%.3f" % (epoch, cnt, np.mean(g_loss[np.where(g_loss)]), time.time() - st)), train_id
+                print("%d %d Loss=%.8f Time=%.3f" % (epoch, cnt, np.mean(g_loss[np.where(g_loss)]), time.time() - st)), train_id, '\n', G_current_d, G_current_g - G_current, G_current, G_current_g#, abs(lg0 - lg).mean()#, '\n', lo, '\n', lg, '\n', '=' * 40
             
         # validation after each epoch
         v_start = time.time()
@@ -251,7 +279,7 @@ def main():
         for i in range(len(valid_in_files)):
             in_path = valid_in_files[i]
             gt_path = valid_gt_files[i]
-            loss = validate(in_path, gt_path, sess, G_loss, out_image, in_image, gt_image)
+            loss = validate(in_path, gt_path, sess, L1_loss, out_image, in_image, gt_image)
             losses += loss,
         summary = sess.run(summary_op, feed_dict={v_loss:np.mean(losses)})
         writer_val.add_summary(summary, count)
